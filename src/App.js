030832ac5a67845ca1f40a92faa0f5cba3372046
Kenneth Aladi <kennethmichael11@gmail.com>
2020-04-26 10:59:33 +0100

    Created the structure for the components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 
+import Cards from "./components/Cards";
+import Chart from "./components/Chart";
+import CountryPicker from "./components/CountryPicker";
+
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>App</h1>
+        <Cards />
+        <Chart />
+        <CountryPicker />
       </div>
     );
   }
