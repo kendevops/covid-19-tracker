b318e5c39e0b681c20698c1ee7226d6159fe2811
Kenneth Aladi <kennethmichael11@gmail.com>
2020-05-03 10:20:09 +0100

    Created the footer part

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="copyright">
+      <div className="up" id="up">
+        <i className="fas fa-chevron-up"></i>
+      </div>
+      <p>&copy; 2020 Kenneth Aladi</p>
+    </footer>
+  );
+};
