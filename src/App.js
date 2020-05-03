0edd404402b007e3a1d61a49d403f1c65193a630
Kenneth Aladi <kennethmichael11@gmail.com>
2020-05-03 10:24:38 +0100

    Finished the footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Cards, Chart, CountryPicker } from "./components";
+import { Cards, Chart, CountryPicker, Footer } from "./components";
 import styles from "./App.module.css";
 import { fetchData } from "./api";
 import image from "./img/image.png";
@@ -32,6 +32,7 @@ class App extends React.Component {
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} />
+        <Footer />
       </div>
     );
   }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,10 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="copyright">
-      <div className="up" id="up">
-        <i className="fas fa-chevron-up"></i>
-      </div>
       <p>&copy; 2020 Kenneth Aladi</p>
     </footer>
   );
 };
+
+export default Footer;
